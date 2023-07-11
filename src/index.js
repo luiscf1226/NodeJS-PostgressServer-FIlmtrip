511a4f4077ffe267822ba7fd2ef4888c6c436d6a
Luis Flores <luiscf1226@gmail.com>
2023-07-10 22:36:18 -0600

    fixing start script

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ app.use(express.urlencoded({extended: false}));
 //routes
 app.use(require('./routes/index'));
 //
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('Server on port 3000');
