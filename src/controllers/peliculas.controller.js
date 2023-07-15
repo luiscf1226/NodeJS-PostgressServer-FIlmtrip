5160ca3cf19ffda94b4654091612d4a521f3f17a
Daniel Sagastume <daniesbw@unitec.edu>
2023-07-15 15:52:42 -0600

    error handling in peliculas

--- src/controllers/peliculas.controller.js
+++ src/controllers/peliculas.controller.js
@@ -1,18 +1,26 @@
-const {Pool}=require('pg');
+const { Pool } = require('pg');
 
-const pool=new Pool({
-    host:'containers-us-west-91.railway.app',
-    user:'postgres',
-    password:'hZiBIOiaDXOVKUJqoc80',
-    database:'railway',
-    port:'7623'
+const pool = new Pool({
+    host: 'containers-us-west-91.railway.app',
+    user: 'postgres',
+    password: 'hZiBIOiaDXOVKUJqoc80',
+    database: 'railway',
+    port: '7623'
 });
 
-const getPeliculas= async (req,res)=>{
-    const response= await pool.query('select * from "Peliculas"');
-    const rows = response.rows; // Obtain the rows from the result
+const getPeliculas = async (req, res) => {
+    try {
+        const response = await pool.query('select * from "Peliculas"');
+        const rows = response.rows; // Obtain the rows from the result
 
-    res.json(rows);
+        res.json(rows);
+
+    } catch (error) {
+        res.status(400)
+        res.send(error.message)
+        console.log(error.message)
+    }
+
 }
 
 const getPelicula = async (req, res) => {
@@ -20,16 +28,17 @@ const getPelicula = async (req, res) => {
     const { idPelicula } = req.params;
 
     try {
-        
+
         const result = await pool.query('SELECT * FROM "Peliculas" WHERE "idPelicula" = $1'
             , [idPelicula])
 
-            const rows = result.rows; // Obtain the rows from the result
+        const rows = result.rows; // Obtain the rows from the result
 
-            res.json(rows);
+        res.json(rows);
     } catch (error) {
         res.status(400)
         res.send(error.message)
+        console.log("error en get pelicula: "+error.message)
     }
 }
 
@@ -47,12 +56,13 @@ const getPeliculaImagen = async (req, res) => {
     } catch (error) {
         res.status(400)
         res.send(error.message)
+        console.log(error.message);
     }
 }
 
-module.exports={
+module.exports = {
     getPeliculas,
     getPelicula,
     getPeliculaImagen
-    
+
 }
